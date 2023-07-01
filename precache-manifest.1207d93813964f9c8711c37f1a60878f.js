3e0774a8918468ce5cb77475823f7cc49ef57651
Pragya Gupta <83855575+pragya14gupta@users.noreply.github.com>
2023-07-01 10:23:25 +0530

    Updates

---
+++ precache-manifest.1207d93813964f9c8711c37f1a60878f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b73e6eee343b30ca409652915489bdd",
+    "url": "/http//:pragya14gupta.github.io/expense-tracker/index.html"
+  },
+  {
+    "revision": "50474a4653aae589051f",
+    "url": "/http//:pragya14gupta.github.io/expense-tracker/static/css/main.3e8f2a80.chunk.css"
+  },
+  {
+    "revision": "130c33d1fbe59508e2dd",
+    "url": "/http//:pragya14gupta.github.io/expense-tracker/static/js/2.cdcf560b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//:pragya14gupta.github.io/expense-tracker/static/js/2.cdcf560b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50474a4653aae589051f",
+    "url": "/http//:pragya14gupta.github.io/expense-tracker/static/js/main.384e961c.chunk.js"
+  },
+  {
+    "revision": "b9ea6e3fb8276c6ca601",
+    "url": "/http//:pragya14gupta.github.io/expense-tracker/static/js/runtime-main.0c9556ff.js"
+  },
+  {
+    "revision": "b5218d0a7ef9b78cd916fb2a625bc259",
+    "url": "/http//:pragya14gupta.github.io/expense-tracker/static/media/logo.b5218d0a.png"
+  }
+]);
